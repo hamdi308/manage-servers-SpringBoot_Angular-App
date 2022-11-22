b151f87a906ea5f7cd6b53b4bcf42d3f3f9a3f6a
hamdi hadda <81825150+hamdi308@users.noreply.github.com>
2022-11-22 12:41:51 +0100

    fetch ping and getserver api

--- frontend/src/app/service/server.service.ts
+++ frontend/src/app/service/server.service.ts
@@ -6,8 +6,11 @@ export class ServerService {
 
   private readonly apiUrl = 'any';
   constructor(private http: HttpClient) { }
-  servers$ = <Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/server/list`).pipe(tap(console.log), catchError(this.handleError))
+  servers$ = <Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/server/list`).pipe(tap(console.log), catchError(this.handleError));
   save$ = (server: Server) => { <Observable<CustomResponse>> this.http.post<CustomResponse>(`${this.apiUrl}/server/save`,server).pipe(tap(console.log), catchError(this.handleError)) };
+  ping$ = (ipAdresse: string) => { <Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAdresse}`).pipe(tap(console.log), catchError(this.handleError)) };
+  server$ = (id: string) => { <Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/server/get/${id}`).pipe(tap(console.log), catchError(this.handleError)) };
+
   handleError(handleError: any): Observable<never> {
     return throwError(() => 'Method not implemented.') ;
   }
